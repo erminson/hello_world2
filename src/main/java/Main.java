d2d1c0949fe3102f3423e07f91fc97e84a0bd48c
Lev Erminson <erminson@gmail.com>
2021-09-06 15:32:18 +0300

    Add comma in output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     private static final String ENV_NAME = "STR";
     public static void main(String[] args) {
         String value = System.getenv(ENV_NAME);
-        System.out.println("Hello " + value);
+        System.out.println("Hello, " + value);
     }
 }
