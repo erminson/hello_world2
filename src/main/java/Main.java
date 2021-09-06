e50d65ed83dac51e994aa6f943179eb9421a6a3e
Lev Erminson <erminson@gmail.com>
2021-09-06 10:47:47 +0300

    Implement reading env variable STR

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    private static final String ENV_NAME = "STR";
+    public static void main(String[] args) {
+        String value = System.getenv(ENV_NAME);
+        System.out.println(value);
+    }
+}
